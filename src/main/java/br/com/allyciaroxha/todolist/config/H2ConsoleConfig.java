73237824b9bf8e145b4b87ba8e00ec5f2a079692
allycia <allyciarocha@ufpi.edu.br>
2026-01-04 17:11:31 -0300

    feat: controller, banco de dados e repository

---
+++ src/main/java/br/com/allyciaroxha/todolist/config/H2ConsoleConfig.java
@@ -0,0 +1,8 @@
+package br.com.allyciaroxha.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2ConsoleConfig {
+    // Configuração padrão do Spring Boot deve ser suficiente
+}
