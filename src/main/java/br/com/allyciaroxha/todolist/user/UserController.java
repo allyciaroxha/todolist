3581ebf2eb3fce50d3a352f3aa34fb22bea28e3f
allycia <allyciarocha@ufpi.edu.br>
2026-01-06 20:56:48 -0300

    feat: cadastro de tarefa e filtro

--- src/main/java/br/com/allyciaroxha/todolist/user/UserController.java
+++ src/main/java/br/com/allyciaroxha/todolist/user/UserController.java
@@ -1,16 +1,14 @@
 package br.com.allyciaroxha.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -25,16 +23,17 @@ public class UserController {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            //mensagem de erro
-            //status code
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
-        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
 
-        userModel.setPassword(passwordHashed);
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
     }
 }
